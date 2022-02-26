9c06ec78794d4d979a8e5327aa7492237cffc985
HynDuf <chauhuynhnoc@gmail.com>
2022-02-26 17:48:56 +0700

    Add FPS to smooth and slow down movements

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,26 @@
 #include <SDL.h>
 
 Game *game = nullptr;
+const int FPS = 60;
+const int frameMaxDelay = 1000 / FPS;
 
+Uint32 frameStart;
+int frameTime;
 int main(int argc, char *argv[])
 {
     game = new Game();
     game->init("Knighty", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
     while (game->running())
     {
+        frameStart = SDL_GetTicks();
         game->handleEvents();
         game->update(); 
         game->render();
-
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < frameMaxDelay)
+        {
+            SDL_Delay(frameMaxDelay - frameTime);
+        }
     }
     game->clean();
     return 0;
